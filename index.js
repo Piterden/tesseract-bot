946ceb2df070f58c8af57ef698de61922980f499
Denis Efremov <efremov.a.denis@gmail.com>
2018-02-09 02:15:11 +0300

    Cleaned

--- index.js
+++ index.js
@@ -2,51 +2,16 @@ require('dotenv').load()
 const fs = require('fs')
 const path = require('path')
 const https = require('https')
-// const express = require('express')
 const ocr = require('tesseract.js')
-// const colors = require('colors/safe')
 const Telegraf = require('telegraf')
 
 
 const { BOT_NAME, BOT_TOKEN } = process.env
 
-// WEBHOOK_DOMAIN, WEBHOOK_PATH, WEBHOOK_PORT, IMAGES_DIR, WEBHOOK_CERT, WEBHOOK_KEY,
-
-// const url = `https://${WEBHOOK_DOMAIN}:${WEBHOOK_PORT}/`
-// const tlsOptions = {
-//   key: fs.readFileSync(path.resolve(WEBHOOK_KEY)),
-//   cert: fs.readFileSync(path.resolve(WEBHOOK_CERT)),
-// }
-
 const bot = new Telegraf(BOT_TOKEN, {
   telegram: { webhookReply: false },
   username: BOT_NAME,
 })
-
-// const server = express()
-// server.use(bot.webhookCallback(`/${WEBHOOK_PATH}`))
-// server.use(`/${IMAGES_DIR}`, express.static(IMAGES_DIR))
-// bot.telegram.setWebhook(`${url}${WEBHOOK_PATH}`, tlsOptions.cert)
-// https.createServer(tlsOptions, server).listen(WEBHOOK_PORT, WEBHOOK_DOMAIN)
-
-/**
- * Log middleware
- */
-// bot.use(async ({ message, from }, next) => {
-//   const start = Date.now()
-
-//   await next()
-
-//   console.log(`
-// ${colors.white.bgBlue('=========================================')}
-// ${JSON.stringify(message, null, '  ')}
-// ${colors.yellow('-----------------------------------------')}
-// ${JSON.stringify(from, null, '  ')}
-// ${colors.yellow('-----------------------------------------')}
-// Response time: ${colors.white.bgRed.bold(Date.now() - start)} ms
-// ${colors.white.bgBlue('=========================================')}
-// `)
-// })
 
 const download = (source, destination) => new Promise((resolve, reject) => {
   const file = fs.createWriteStream(destination)
